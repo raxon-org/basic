9ffcdf1f3e7897a4185259653a2c79f3d1f4ea2a
remco van der Velde <remco.pc@outlook.com>
2025-01-07 19:28:57 +0100

    boot init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -1331,7 +1331,11 @@ trait Main {
                 'source' => $source
             ]);
         }
-        $dir_lock = $object->config('framework.dir.temp') . $object->config(Config::POSIX_ID) . '/Lock/';
+        $dir_lock = $object->config('framework.dir.temp') .
+            $object->config(Config::POSIX_ID) .
+            $object->config('ds') .
+            'Lock' .
+            $object->config('ds');
         ddd($dir_lock);
         Dir::create($dir_lock, Dir::CHMOD);
         foreach($read->get('Boot.start.once') as $command){

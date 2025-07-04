57f9de443c69a971fa1441dae81becf94c4b7a1d
remco van der Velde <remco.pc@outlook.com>
2025-07-04 18:36:18 +0200

    Add debug output for read data in apache2_site_create method

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -427,8 +427,8 @@ trait Main {
             }
             $is_missing = false;
             $object->set('options', $options);
-            $read = $parse->compile($read, $object->data());
             d($read);
+            $read = $parse->compile($read, $object->data());            
             ddd($read);
             $number = sprintf("%'.03d", File::count($dir_available));
             $url = $dir_available . $number . '-' . str_replace('.', '-', $options->server->name) . $object->config('extension.conf');

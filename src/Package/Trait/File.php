f28417f28355614d410a300aa25662b83150c011
Remco van der Velde <remco@universeorange.com>
2025-11-13 00:20:26 +0100

    pp

--- src/Package/Trait/File.php
+++ src/Package/Trait/File.php
@@ -26,6 +26,9 @@ trait File {
         if(!property_exists($options, 'directory')){
             throw new Exception('Option -directory not set');
         }
+        if(!property_exists($options, 'disable-symlink')){
+            throw new Exception('Option -disable-symlink not set and could cause a recursion in the system which causes a kill after rubbishing the memory...');
+        }
         $dir = new Dir();
         $list = $dir->read($options->directory, true);
         foreach($list as $file){

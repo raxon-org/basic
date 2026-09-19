c600cf5fbe262fca655713765bedc5f0ed64451c
Remco van der Velde <remco@universeorange.com>
2026-09-19 20:08:01 +0200

    init

--- src/Package/Trait/Install.php
+++ src/Package/Trait/Install.php
@@ -83,6 +83,9 @@ trait Install {
                                     $parse = new Parse($clone, $data, $flags, $clone_options->data());
                                     $read = File::read($file->url);
                                     $content = $parse->compile($read, $data);
+                                    if(stristr($file->url, 'dialog')){
+                                        d($content);
+                                    }
                                     if($patch !== null) {
                                         File::delete($file->target);
                                     }
@@ -125,7 +128,7 @@ trait Install {
                 Dir::create($file->target, Dir::CHMOD);
                 File::permission($object, [
                     'target' => $file->target,
-                ]);                 
+                ]);
             }
         }
     }
